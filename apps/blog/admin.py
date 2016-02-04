d96bd2776ad31b794d44c9db01f56d9db3422f20
mohit-pc <mohit-pc>
2016-02-05 00:27:11 +0530

    added Blog models

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,3 +1,12 @@
+"""Importing Admin."""
+from apps.blog.models import AnswerComments, Answers, QuestionComments, Questions, UserProfile
+
 from django.contrib import admin
 
 # Register your models here.
+
+admin.site.register(UserProfile)
+admin.site.register(Questions)
+admin.site.register(QuestionComments)
+admin.site.register(Answers)
+admin.site.register(AnswerComments)
